75b9a4cdf44b6d1c90490b3b7740a117d69d03fd
DoriTran <tranquocdong308@gmail.com>
2022-08-29 16:35:09 +0700

    Change upload file

--- app/src/services/employee/addNewAvatar.js
+++ app/src/services/employee/addNewAvatar.js
@@ -1,12 +1,18 @@
 angular.module('app').service('addNewAvatar', function($http) {
     this.post = function(no, avatar) {
-        return $http({
-            method: 'POST',
-            url: API_URL + '/profile/avatar/employeeNo=' + no,
-            data: {file: avatar},
-            headers: {
-                'Content-type': "multipart/form-data"
-            }
-        });
+        console.log(avatar)
+        let formdata = new FormData();
+        formdata.append('file', avatar);
+
+
+        return    $http({
+                method: 'POST',
+                url: API_URL + '/profile/avatar/employeeNo=' + no, formdata,
+                headers: {'Content-Type': 'multipart/form-data'},
+                data: formdata,
+                transformRequest: function(data, headersGetterFunction) {
+                    return data; // do nothing! FormData is very good!
+                }
+            })
     }
 })
